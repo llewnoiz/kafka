6b92d61ec72d9ea72ef9d92d13624bf1feebbf06
HWK\hyunmin.song <hyunmin.song@bespinglobal.com>
2023-07-24 18:08:47 +0900

    fixed: public kafka

--- msk/kafka_cluster.tf
+++ msk/kafka_cluster.tf
@@ -11,8 +11,8 @@ module "msk_cluster" {
   kafka_version          = "2.8.1"
   number_of_broker_nodes = 2
 
-  # broker_node_client_subnets  = module.vpc.public_subnets
-  broker_node_client_subnets  = module.vpc.private_subnets
+  broker_node_client_subnets  = module.vpc.public_subnets
+  # broker_node_client_subnets  = module.vpc.private_subnets
   broker_node_instance_type   = local.instance_type
   broker_node_security_groups = [module.security_group.security_group_id]
 
@@ -24,6 +24,10 @@ module "msk_cluster" {
   #     scram = true 
   #   }    
   # }
+  
+  client_authentication = {
+    sasl = { scram = true }
+  }
   broker_node_connectivity_info = {
     public_access = { type = local.public_access }
   }
@@ -32,7 +36,7 @@ module "msk_cluster" {
   configuration_server_properties = {
     "auto.create.topics.enable" = true
     "delete.topic.enable"       = true    
-    # "allow.everyone.if.no.acl.found"=false
+    "allow.everyone.if.no.acl.found"=false
     # "default.replication.factor"=3
     # "min.insync.replicas"=2
     # "num.io.threads"=8
@@ -47,8 +51,8 @@ module "msk_cluster" {
     # "zookeeper.session.timeout.ms"=18000
   }
 
-  # create_scram_secret_association          = true
-  # scram_secret_association_secret_arn_list = [for x in aws_secretsmanager_secret.this : x.arn]
+  create_scram_secret_association          = true
+  scram_secret_association_secret_arn_list = [for x in aws_secretsmanager_secret.this : x.arn]
 
   tags = local.tags
 
